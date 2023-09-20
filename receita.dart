f810a863db788670417adadd34c45129b28f1b03
Davi <128321234+dav1senpai@users.noreply.github.com>
2023-09-20 20:46:55 -0300

    receita.dart

---
+++ receita.dart
@@ -0,0 +1,37 @@
+class Receita{
+  String label;
+  String imageUrl;
+  // TODO: Add servings and ingredients here
+  Recipe(
+  this.label,
+  this.imageUrl,
+  );
+}
+static List<Receita> samples = [
+  Receita(
+    'Spaghetti and Meatballs',
+    'assets/2126711929_ef763de2b3_w.jpg',
+  ),
+  Receita(
+    'Tomato Soup',
+    'assets/27729023535_a57686c1be.jpg',
+),
+Receita(
+'Grilled Cheese',
+'assets/3187380632_5056654a19_b.jpg',
+),
+Receita(
+'Chocolate Chip Cookies',
+'assets/15992182771_b92f4cc08a_b.jpg',
+),
+Receita (
+  'Taco Salad',
+'assets/8533381643_a31a99e8a6_c.jpg',
+),
+
+Receita (
+'Hawaiian Pizza',
+'assets/15452035777_294cefced5_c.jpg',
+),
+];
+  // TODO: Add Ingredientes() here
